c7c1096502d7c3852f565c5d94bd610585142986
Lazar Matic <lazar.matic@menu.app>
2022-03-20 22:19:57 +0100

    Some useless libs removed

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,6 @@ module.exports = {
   projects: [
     ,
     '<rootDir>/apps/my-home-library',
-    '<rootDir>/libs/small-app',
-    '<rootDir>/libs/small-app-v2',
-    '<rootDir>/libs/small-app-v3',
     '<rootDir>/apps/elec-test',
     '<rootDir>/apps/my-home-library-express',
   ],
--- libs/small-app-v2/src/index.ts
+++
@@ -1,2 +0,0 @@
-import App from './lib/app/main.vue';
-export default App;
