25d9c1a0a93794b138ed345192fa5eb839d4050f
lazix95 <lazix95@live.com>
2022-09-23 14:28:38 +0200

    [Bookshwlf-app] Bookshelf-app frontend validation - work in progress

--- apps/bookshelf-app/src/pages/Register/Register.vue
+++ apps/bookshelf-app/src/pages/Register/Register.vue
@@ -1,58 +1,23 @@
 <template>
-  <v-container
-    fill-height
-    fluid
-    style="background-color: var(--background-poop)"
-  >
+  <v-container fill-height fluid style="background-color: var(--background-poop)">
     <v-row justify="center">
       <v-col class="pt-0 text-center" cols="12" sm="6" md="5" lg="4" xl="3">
-        <p class="title">Sing in to your library</p>
-        <p class="subtitle">Please enter your email and password</p>
+        <p class="title">Register  your account</p>
+        <p class="subtitle">Please make sure all fields are filled in correctly.</p>
+
         <v-divider class="pb-5 mt-3"></v-divider>
-        <v-text-field
-          dense
-          v-model="firstName"
-          outlined
-          label="First Name"
-          :error-messages="errors.firstName"
-        />
-        <v-text-field
-          dense
-          v-model="lastName"
-          outlined
-          label="Last Name"
-          :error-messages="errors.lastName"
-        />
-        <v-text-field
-          dense
-          v-model="email"
-          outlined
-          label="E-mail"
-          :error-messages="errors.email"
-        />
-        <v-text-field
-          type="password"
-          dense
-          v-model="password"
-          outlined
-          label="Password"
-          :error-messages="errors.password"
-        />
-        <v-text-field
-          type="password"
-          dense
-          v-model="confirmPassword"
-          outlined
-          label="Confirm Password"
-          :error-messages="errors.confirmPassword"
-        />
-        <v-btn
-          :loading="submitLoading"
-          width="100%"
-          color="primary"
-          @click="onSignUp"
-          >Submit</v-btn
-        >
+
+        <v-form ref='form'>
+          <v-text-field dense v-model="firstName" outlined label="First Name" :rules='firstNameRules'/>
+          <v-text-field dense v-model="lastName" outlined label="Last Name" :rules='lastNameRules' />
+          <v-text-field dense v-model="email" outlined label="E-mail" :rules='emailRules' />
+          <v-text-field type="password" dense v-model="password" outlined label="Password" :rules='passwordRules' />
+          <v-text-field type="password" dense v-model="confirmPassword" outlined label="Confirm Password" :rules="[(value) => value === password || `Confirm Password must be same as Password`]" />
+
+          <v-btn :loading="submitLoading" width="100%" color="primary" @click="onSignUp">
+            Submit
+          </v-btn>
+        </v-form>
       </v-col>
     </v-row>
   </v-container>
@@ -62,6 +27,12 @@
 import { Component, Vue } from 'vue-property-decorator';
 import { RegisterApiPayload } from '../../api/auth';
 import { RegisterErrors } from './RegisterPageTypes';
+import {
+  ruleAtLeastOneNumber,
+  ruleAtLeastOneUppercase,
+  ruleEmailRequiredAndValid,
+  ruleFieldRequiredMinMax, ruleSameAsField
+} from '../../validation-rules/text-validations';
 
 @Component
 export default class Login extends Vue {
@@ -73,11 +44,27 @@ export default class Login extends Vue {
   private submitLoading = false;
   private errors: RegisterErrors = {};
 
+  private firstNameRules = ruleFieldRequiredMinMax('First Name', 3, 50);
+  private lastNameRules = ruleFieldRequiredMinMax('Last Name', 3, 50);
+  private emailRules = ruleEmailRequiredAndValid();
+  private passwordRules = [...ruleFieldRequiredMinMax('Password', 6, 50), ...ruleAtLeastOneUppercase('Password'), ...ruleAtLeastOneNumber('password')];
+  get confirmPasswordRules(): unknown {
+    return ruleSameAsField
+  }
+
   clearErrors(): void {
     this.errors = {};
   }
 
+  validate () {
+    const form = this.$refs.form as any;
+    if (this.$refs.form) return form.validate();
+    return false;
+  }
+
   onSignUp(): void {
+    console.log(this.validate());
+    return
     this.clearErrors();
     const payload: RegisterApiPayload = {
       firstName: this.firstName,
@@ -88,16 +75,11 @@ export default class Login extends Vue {
     };
 
     this.submitLoading = true;
-    this.$store
-      .dispatch('auth/register', payload)
-      .then(() => {
+    this.$store.dispatch('auth/register', payload).then(() => {
         this.submitLoading = false;
         this.$router.push({ name: 'Books.vue' });
-      })
-      .catch((err) => {
-        console.log('err => ');
-        if (err.response.data.validationMessages)
-          this.errors = err.response.data.validationMessages;
+      }).catch(() => {
+        //if (err.response.data.validationMessages) this.errors = err.response.data.validationMessages;
         this.submitLoading = false;
       });
   }
---
+++ apps/bookshelf-app/src/validation-rules/text-validations.ts
@@ -0,0 +1,34 @@
+export function ruleFieldRequiredMinMax(filedName: string, minLength = 0, maxLength = 20): Array<unknown> {
+  return [
+    (value: string) => !!value || `${filedName} is required`,
+    (value: string) => (value && value.length >= minLength) || `${filedName} must be more than ${minLength} characters`,
+    (value: string) => (value && value.length <= maxLength) || `${filedName} must be less than ${maxLength} characters`,
+  ]
+}
+
+export function ruleEmailRequiredAndValid(): Array<unknown>{
+  return [
+    (value: string) => !!value || 'E-mail is required',
+    (value: string) => /.+@.+\..+/.test(value) || 'E-mail must be valid',
+  ]
+}
+
+export function ruleAtLeastOneUppercase(fieldName: string): Array<unknown> {
+  return [
+    (value: string) => (value && (value.match(/[A-Z]/g) || []).length > 0) || `${fieldName} must include at least one Uppercase letter`,
+  ]
+}
+
+export function ruleAtLeastOneNumber(fieldName: string): Array<unknown> {
+  return [
+    (value: string) => (value && (value.match(/[0-9]/g) || []).length > 0) || `${fieldName} must include at least one Number`,
+  ]
+}
+
+export function ruleSameAsField(fieldName: string, sameAsField: string, refValue: string): Array<unknown> {
+  return [
+    [(value: string) => (value === refValue) || `${fieldName} must be same as ${sameAsField}`],
+  ]
+}
+
+
